40ff4309dc91917ef0dda5ed03a053ab6fd5e26e
Isha Agarwal <isha.agarwal.int@geeksforgeeks.org>
2024-03-20 21:34:05 +0530

    updated backend url

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://codeeditor-nbl0.onrender.com";
+axios.defaults.baseURL = "https://codeeditor-nbl0.onrender.com/api";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
